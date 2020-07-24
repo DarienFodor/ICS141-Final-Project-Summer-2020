756afe74ba310405fbe040fb26bea1a8d88a9181
DarienFodor <68536437+DarienFodor@users.noreply.github.com>
2020-07-24 15:02:03 -0500

    Delete TaxTest.java

--- ics141/mainproject/TaxTest.java
+++
@@ -1,8 +0,0 @@
-package ics141.mainproject;
-
-public class TaxTest {
-	public static void main(String[] args) {
-	TaxInfo John = new TaxInfo(1000000.0, "Single");
-	System.out.println(John.tax_bracket());
-	}
-}
